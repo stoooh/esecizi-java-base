c1c7869144c0288f82e7ffc93fbf7dc101f29a70
stoooh <gabriele.stoian@hotmail.com>
2023-03-10 12:03:20 +0100

    done exercise 13

--- src/javabasics/_13/Main.java
+++ src/javabasics/_13/Main.java
@@ -22,7 +22,21 @@ public class Main {
         System.out.println("Exercise 1:");
         ArrayList<Integer> numbers = new ArrayList<>();
 
-        //Write your code here
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(6);
+        numbers.add(8);
+        numbers.add(11);
+        numbers.add(65);
+        numbers.add(34);
+        numbers.add(95);
+
+        System.out.println(numbers.get(0));
+        numbers.remove(0);
+        numbers.remove(1);
+        numbers.remove(2);
+        System.out.println(numbers);
+
     }
 
     /**
@@ -32,14 +46,25 @@ public class Main {
      * <p>
      * And a list of Doubles called 'menuPrices'
      * <p>
-     * The indexes of these two lists correspond to eachother
+     * The indexes of these two lists correspond to each other
      * <p>
      * Create a print statement to output the name and price of each
      * menu item correspond
      */
     public static void exercise2() {
-        System.out.println("\nExercise 1:");
-        //Write your code here
+        System.out.println("\nExercise 2:");
+        ArrayList<String>menuItems= new ArrayList<>();
+        ArrayList<Double>menuPrices= new ArrayList<>();
+
+        menuItems.add("hamburger");
+        menuPrices.add(5.99);
+        menuItems.add("hotdog");
+        menuPrices.add(8.56);
+        menuItems.add("toast");
+        menuPrices.add(4.50);
+
+        System.out.println("the menu with " + menuItems.get(0) + " cost "  + "€" +menuPrices.get(0));
+
     }
 
     /**
@@ -57,6 +82,7 @@ public class Main {
         System.out.println("\nExercise 3:");
         int[] items = new int[]{5, 10404, -234348, -53, 9, 91, 92, 34534, 12334, 1435};
 
-        //Write your code here
+        int sum = items[0] +items[1] +items[2] +items[3] +items[4] +items[5] +items[6] +items[7] +items[8] +items[9];
+        System.out.println(sum);
     }
 }
