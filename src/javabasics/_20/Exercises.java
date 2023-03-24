417a88d3983b8979b5ba3fe0f6fce3d88d4f1f38
stoooh <gabriele.stoian@hotmail.com>
2023-03-24 14:49:02 +0100

    exercise 20 done

--- src/javabasics/_20/Exercises.java
+++ src/javabasics/_20/Exercises.java
@@ -1,5 +1,8 @@
 package javabasics._20;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercises {
     public static void main(String[] args) {
         exercise1();
@@ -8,34 +11,71 @@ public class Exercises {
 
     /**
      * 1: Copy over the student class
-     *
-     *    Make sure there are 5 fields inside, if they are not simply create some more.
-     *    make every field private first.
-     *
-     *    Then create getters and setters for each of these 5 fields.
-     *
-     *    Once ready use all 5 below
+     * <p>
+     * Make sure there are 5 fields inside, if they are not simply create some more.
+     * make every field private first.
+     * <p>
+     * Then create getters and setters for each of these 5 fields.
+     * <p>
+     * Once ready use all 5 below
      */
     private static void exercise1() {
         System.out.println("Exercise 1:");
 
         // Write and use your 5 getters and setters!
+        ArrayList<Student> students = new ArrayList<>();
+        Student student = new Student(19, "Piero", "maschio", "A", "matematica");
+        // qui ho voluto provare il metodo creando un array
+
+        students.add(new Student(19, "Piero", "maschio", "A", "matematica"));
+        //qui ho provato un altro metodo con .add
+
+
+        student.setName("Maria");
+        System.out.println(student.getName());
+        student.setAge(13);
+        System.out.println(student.getAge());
+        student.setGender("girl");
+        System.out.println(student.getGender());
+
+
+        System.out.println(students.get(0).getAge() + " " +
+                students.get(0).getName() + " " +
+                students.get(0).getGender() + " " +
+                students.get(0).getSection() + " " +
+                students.get(0).getSubject());
+
     }
 
     /**
      * 2: Create 3 setter constraints for the student class
-     *
-     *    Think about what reasonably constraints are?
-     *
-     *    Should a student be able to be a negative age?
-     *
-     *    Create a counter for one of the getters, try to think about
-     *    what other code could be inside a getter? Why not write it?
+     * <p>
+     * Think about what reasonably constraints are?
+     * <p>
+     * Should a student be able to be a negative age?
+     * <p>
+     * Create a counter for one of the getters, try to think about
+     * what other code could be inside a getter? Why not write it?
      */
     private static void exercise2() {
         System.out.println("\nExercise 2:");
 
-        //Write your code here
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(19, "Piero", "man", "A", "math"));
+        students.add(new Student(16, "Angelo", "man", "B", "math"));
+        students.add(new Student(24, "Paolo", "man", "C", "math"));
+        students.add(new Student(14, "Lu", "girl", "C", "math"));
+        //ho un piccolo problema con il codice. Tutto funziona ma quando lo avvio lo studente di
+        // nome Lu mi da null come valore, ma solo alla prima riga, perche??
+
+        for (int i = 0; i < students.size(); i++) {          //ho preferito creare un for per far si che mi stampasse tutti gli studenti dell'array
+            System.out.println(students.get(i).getAge() + " " +
+                    students.get(i).getName() + " " +
+                    students.get(i).getGender() + " " +
+                    students.get(i).getSection() + " " +
+                    students.get(i).getSubject());
+        }
+        System.out.println("the counter of the string name is " + students.get(0).getCounter(students));
     }
 
 }
---
+++ src/javabasics/_20/Student.java
@@ -0,0 +1,82 @@
+package javabasics._20;
+
+import java.util.ArrayList;
+
+public class Student {
+    private int age;
+    private String name;
+    private String gender;
+    private String section;
+    private String subject;
+
+    private int counter = 0;
+
+    public Student(int age, String name, String gender, String section, String subject) {
+        setAge(age);
+        setName(name);
+        setGender(gender);
+        setSection(section);
+        setSubject(subject);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setAge(int age) {
+        if (age < 15) {
+            System.out.println(name + " can't access in this class because is too young");
+            this.age = age; //ho lasciato this anche qui in modo che mi stampasse lo stesso il valore
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void setName(String name) {
+        if (name.length() <= 2) {
+            System.out.println( name + " can't access in this class because her/his name is too short");
+            this.name = name;    //ho lasciato this anche qui in modo che mi stampasse lo stesso il valore
+        } else {
+            this.name = name;
+        }
+        counter += 1;
+    }
+
+    public void setGender(String gender) {
+        if (gender.equals("girl")) {
+            System.out.println(name + " can't access in this class because is a girl");
+            this.gender = gender;  //ho lasciato this anche qui in modo che mi stampasse lo stesso il valore
+        } else {
+            this.gender = gender;
+        }
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getCounter(ArrayList array) {
+        counter = array.size();
+        return counter;
+    }
+}
